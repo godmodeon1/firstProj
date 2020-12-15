fcb56a7791e30194cd872655bdb70f835bdd7050
godmodeon1 <openoffline2@gmail.com>
2020-12-15 21:39:16 +0300

    -bags: hide not all illustration elems; +imgView

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function hide(push) {
     //CONDITIONS
     let CONDITIONS = ["0", ".empty", ".web, .graphics, .illustration, .audio, .video",
                   "1", ".graphics, .illustration, .audio, .video", ".web",
-                  "2", ".web, .illustation, .audio, .video", ".graphics",
+                  "2", ".web, .illustration, .audio, .video", ".graphics",
                   "3", ".graphics, .web, .illustration, .audio", ".video",
                   "4", ".graphics, .web, .audio, .video", ".illustration",
                   "5", ".graphics, .web, .illustration, .video", ".audio"]
@@ -37,6 +37,44 @@ function hide(push) {
     let selector = 1 + parseInt(lnk);
     let link = document.querySelector('.filter ul li:nth-child(' + selector +')');
     link.style.color = "#fa6c65";
-
 }
 
+function imageViewer(id) {
+    //close old window if need
+    if (document.querySelector(".imgView") != null) {
+        let toRm = document.querySelector(".imgView");
+        toRm.remove();
+    }
+
+    //get URL
+    element = document.querySelector("#" + id);
+    background = window.getComputedStyle(element).background;
+    imgUrl = background.split('"')[1];
+   
+    //get element position
+    let coords = element.getBoundingClientRect();
+
+    //get current scroll
+    let scroll = window.pageYOffset;
+
+    //get window size and calc image-sizing and proportion
+    let imageProportion =  window.innerWidth / coords.width * 0.5;
+    let imgWidth = coords.width*imageProportion;
+    let imgHeight = coords.height*imageProportion;
+    let topPadding = (window.innerHeight - coords.height*imageProportion) / 2;
+    let topPosition = scroll + topPadding;
+    let leftPosition = (window.innerWidth - imgWidth) / 2;
+
+    //create imgView
+    let view = document.createElement('div');
+    view.classList.add("imgView")
+    view.innerHTML = "<input type='button' alt='='Закрыть'' title='Закрыть' value='X' onclick='rmElem(this.parentNode.className)'>";
+    view.style.cssText = "top:" + topPosition + "px; left:" + leftPosition + "px; width:" + 
+            imgWidth + "px; height:" + imgHeight + "px; background: url(" + imgUrl + "); background-size: cover;";
+    document.body.append(view);
+
+}
+function rmElem(id) {
+    let toRm = document.querySelector("." + id);
+    toRm.remove();
+}
